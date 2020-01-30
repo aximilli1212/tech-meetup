5a5c1b448c99bfb10806d39e35560d25ffa285e4
Aximilli1212 <aximill1212@gmail.com>
2020-01-30 18:14:10 +0000

    set up new prisma api

--- src/index.js
+++ src/index.js
@@ -1,36 +1,17 @@
-// import { GraphQLServer, PubSub } from 'graphql-yoga'
-const { GraphQLServer, PubSub } = require('graphql-yoga')
-import db from './db'
-import Query from './resolvers/Query'
-import Mutation from './resolvers/Mutation'
-import Subscription from './resolvers/Subscription'
-import User from './resolvers/User'
-import Post from './resolvers/Post'
-import Comment from './resolvers/Comment'
+const { prisma } = require('../generated/prisma-client');
+const { GraphQLServer } = require('graphql-yoga');
+const typeDefs = require("../generated/prisma-client/prisma-schema").typeDefs;
 
-import "../prisma";
 
-const pubsub = new PubSub()
-//
+const resolvers = {
+
+}
+
 const server = new GraphQLServer({
-    typeDefs: './src/schema.graphql',
-    resolvers: {
-        Query,
-        Mutation,
-        Subscription,
-        User,
-        Post,
-        Comment
-    },
+    typeDefs: typeDefs,
+    resolvers,
     context: {
-        db,
-        pubsub
-    }
+        prisma,
+    },
 })
-
-server.start(() => {
-    console.log('The server is up!')
-
-})
-
-console.log("pooped");
+server.start(() => console.log('Server is running on http://localhost:4000'))
